c465de82558b462e341e7bef060460ccebc05c13
mixaverros88 <mixalisverros@hotmail.gr>
2020-05-16 12:45:12 +0300

    update mysql docker file

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ docker build -t mixaverross88/java-e-commerce-mysql:1.1 -f Dockerfile.Mysql .
 # Run Mysql Image
 docker run -d -p 3311:3306 --name mysqlapp \
 --network=my_isolated_bridge_network \
--e MYSQL_ROOT_PASSWORD=kdiosk33 mixaverross88/java-e-commerce-mysql:1.1
+-e MYSQL_ROOT_PASSWORD=M!xalis1029 mixaverross88/java-e-commerce-mysql:1.1
 
 # Build java app
 docker build -t mixaverross88/java-e-commerce:3.1 .
